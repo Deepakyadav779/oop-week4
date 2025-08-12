c8337d1ff0392da10fec9a283fb143cc85f8c251
its_deepak.yadav <deepuyadav9603@gmail.com>
2025-08-12 14:39:29 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,23 @@
+package display;
+
+
+package sec4java;
+
+class Student
+{  
+int rollno;  
+String name;  
+float fee;  
+
+Student(int  rollno, String  name, float fee)
+{  
+	this.rollno=rollno; 
+	this.name=name;  
+	this.fee=fee;  
+}  
+
+void display()
+    {
+    System.out.println(rollno+" "+name+" "+fee);
+    }  
+}  
