1f526e66f34308b91fd2bc9a2c9ef4649a2979d3
its_deepak.yadav <deepuyadav9603@gmail.com>
2025-08-12 14:36:30 +0530

    Add files via upload

---
+++ classbox.java
@@ -0,0 +1,48 @@
+class BoxDemo6 
+{ 
+public static void main(String args[]) 
+{
+ 
+ Box mybox1 =   new Box();
+ Box mybox2 = new Box(); 
+ double vol; 
+
+// get volume of first box 
+ vol = mybox1.volume();
+ System.out.println("Volume is " + vol);
+
+ // get volume of second box 
+ vol = mybox2.volume(); 
+ System.out.println("Volume is " + vol); 
+}
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
